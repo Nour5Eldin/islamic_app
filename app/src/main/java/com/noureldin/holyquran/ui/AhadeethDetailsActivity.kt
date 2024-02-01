608b5d75103bee375643af1687605d8858e7bd09
noureldinmahmoud <noureldinmahmoud02@gmail.com>
2024-02-01 21:22:30 +0200

    light & dark mode.

--- app/src/main/java/com/noureldin/holyquran/ui/AhadeethDetailsActivity.kt
+++ app/src/main/java/com/noureldin/holyquran/ui/AhadeethDetailsActivity.kt
@@ -3,6 +3,8 @@ package com.noureldin.holyquran.ui
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.ImageView
 import com.noureldin.holyquran.data.Constant
 import com.noureldin.holyquran.databinding.ActivityAhadeethDetailsBinding
 import com.noureldin.holyquran.model.HadeethModel
@@ -20,6 +22,21 @@ class AhadeethDetailsActivity : AppCompatActivity() {
         }
         binding.AhadeethName.text = hadeeth!!.title
         binding.AhadeethContentTv.text = hadeeth.content
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                // Handle the Up button click
+                onBackPressed()
+                return true
+            }
+            // Add other menu item cases if needed
+            else -> return super.onOptionsItemSelected(item)
+        }
 
     }
 }
--- app/src/main/java/com/noureldin/holyquran/ui/SuraDetailsActivity.kt
+++ app/src/main/java/com/noureldin/holyquran/ui/SuraDetailsActivity.kt
@@ -3,6 +3,7 @@ package com.noureldin.holyquran.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import com.noureldin.holyquran.data.Constant
 import com.noureldin.holyquran.databinding.ActivitySuraDetailsBinding
 import java.io.BufferedReader
@@ -12,30 +13,32 @@ import java.io.InputStreamReader
 
 class SuraDetailsActivity : AppCompatActivity() {
 
-   lateinit var Binding: ActivitySuraDetailsBinding
+    lateinit var Binding: ActivitySuraDetailsBinding
 
-   lateinit var suraName: String
-   lateinit var fileName: String
-   var AyatCount: Int = 0
+    lateinit var suraName: String
+    lateinit var fileName: String
+    var AyatCount: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Binding = ActivitySuraDetailsBinding.inflate(layoutInflater)
         setContentView(Binding.root)
 
-       suraName =  intent.getStringExtra(Constant.SURA_NAME)!!
+        suraName = intent.getStringExtra(Constant.SURA_NAME)!!
         fileName = intent.getStringExtra(Constant.FILE_NAME)!!
-        AyatCount = intent.getIntExtra(Constant.AyatCount,0)
+        AyatCount = intent.getIntExtra(Constant.AyatCount, 0)
 
 
         Binding.contentTv.text = readFileContent()
-        Binding.chapterName.text =  suraName
+        Binding.chapterName.text = suraName
         Binding.chapterNumbersAya.text = AyatCount.toString()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
 
-        Log.e("ActivitySuraDetailsBinding/onCreate",
-            "suraName =${suraName}"+"+ file = ${fileName}"+"numberOfAyat =${AyatCount}")
-
+        Log.e(
+            "ActivitySuraDetailsBinding/onCreate",
+            "suraName =${suraName}" + "+ file = ${fileName}" + "numberOfAyat =${AyatCount}"
+        )
 
 
     }
@@ -44,22 +47,33 @@ class SuraDetailsActivity : AppCompatActivity() {
         var fileContent = ""
         try {
             val inputStream = assets.open(fileName)
-            val reader= BufferedReader(InputStreamReader(inputStream))
-           val fileLines: List<String> = reader.readLines()
-             fileContent = fileLines.joinToString(separator = ""){line ->
-                    val index = fileLines.indexOf(line)+1
+            val reader = BufferedReader(InputStreamReader(inputStream))
+            val fileLines: List<String> = reader.readLines()
+            fileContent = fileLines.joinToString(separator = "") { line ->
+                val index = fileLines.indexOf(line) + 1
 
-                return@joinToString line+ "{${index}}"
+                return@joinToString line + "{${index}}"
             }
 
-            Log.e("SuraDetailsActivity/readFileContent","fileLines = ${fileContent}")
+            Log.e("SuraDetailsActivity/readFileContent", "fileLines = ${fileContent}")
             inputStream.close()
-        }catch (e: IOException){
+        } catch (e: IOException) {
             e.printStackTrace()
         }
         return fileContent
 
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                // Handle the Up button click
+                onBackPressed()
+                return true
+            }
+            // Add other menu item cases if needed
+            else -> return super.onOptionsItemSelected(item)
+        }
 
+    }
 }
